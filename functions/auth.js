c379821f91f7f9455cc10de2ca26644d1885ef8e
AnubhavGitHub07 <prince1dwivedi00@gmail.com>
2026-03-01 17:36:36 +0530

    fix-failed-load-function

--- functions/auth.js
+++ functions/auth.js
@@ -1,10 +1,18 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./private/cl-dev-pk.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.VITE_APP_DATABASE_URL
-});
+const databaseURL = process.env.VITE_APP_DATABASE_URL || `https://${process.env.GCLOUD_PROJECT}.firebaseio.com`;
+
+if (process.env.FUNCTIONS_EMULATOR === "true" || process.env.FIREBASE_CONFIG) {
+  admin.initializeApp({
+    databaseURL: databaseURL
+  });
+} else {
+  const serviceAccount = require("./private/cl-dev-pk.json");
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: databaseURL
+  });
+}
 
 const db = admin.firestore();
 const rtdb = admin.database();
